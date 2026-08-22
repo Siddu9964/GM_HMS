6616c49d97926b34555a4fc6fa15e454474bde22
Siddu9964 <siddeshbm9964@gmail.com>
2026-08-22 13:06:52 +0530

    reception_view SIDEBAR name chnage

--- reception_view/includes/reception_sidebar.php
+++ reception_view/includes/reception_sidebar.php
@@ -83,7 +83,15 @@ $basePath = str_repeat('../', $depth);
                         <a href="<?php echo $basePath; ?>opd_billing_report.php" class="opd-item" data-pd="opd_billing_report"
                             style="--n:3">
                             <i class="fas fa-chart-bar"></i>
-                            <span>Billing Report</span>
+                            <span>OP Billing Report</span>
+                        </a>
+                        <?php endif; ?>
+
+                        <?php if (in_array($_SESSION['role'], ['Admin', 'admin', 'Accountant', 'Receptionist'])): ?>
+                        <a href="<?php echo $basePath; ?>ipd_billing_report.php" class="opd-item" data-pd="ipd_billing_report"
+                            style="--n:4">
+                            <i class="fas fa-chart-bar"></i>
+                            <span>IP Billing Report</span>
                         </a>
                         <?php endif; ?>
                     </div>
